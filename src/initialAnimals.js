1e48d39d668710d57b7191fb8fa577f4f4fa326c
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-03-26 21:01:48 -0600

    Add initialAnimals to initialize characters

---
+++ src/initialAnimals.js
@@ -0,0 +1,45 @@
+import baba from './assets/animals/baba.jpg';
+import daimon from './assets/animals/daimon.jpg';
+import dobu from './assets/animals/dobu.jpg';
+import donraku from './assets/animals/donraku.jpg';
+import goriki from './assets/animals/goriki.jpg';
+import ichimura from './assets/animals/ichimura.jpg';
+import imai from './assets/animals/imai.jpg';
+import kabasawa from './assets/animals/kabasawa.jpg';
+import kakihana from './assets/animals/kakihana.jpg';
+import kuroda from './assets/animals/kuroda.jpg';
+import nagashima from './assets/animals/nagashima.jpg';
+import nikaido from './assets/animals/nikaido.jpg';
+import odokawa from './assets/animals/odokawa.jpg';
+import sekiguchi from './assets/animals/sekiguchi.jpg';
+import shibagaki from './assets/animals/shibagaki.jpg';
+import shirakawa from './assets/animals/shirakawa.jpg';
+import tanaka from './assets/animals/tanaka.jpg';
+import wadagaki from './assets/animals/wadagaki.jpg';
+import yamamoto from './assets/animals/yamamoto.jpg';
+import yano from './assets/animals/yano.jpg';
+
+const initialAnimals = [
+  { name: 'Baba', image: baba },
+  { name: 'Daimon', image: daimon },
+  { name: 'Dobu', image: dobu },
+  { name: 'Donraku', image: donraku },
+  { name: 'Goriki', image: goriki },
+  { name: 'Ichimura', image: ichimura },
+  { name: 'Imai', image: imai },
+  { name: 'Kabasawa', image: kabasawa },
+  { name: 'Kakihana', image: kakihana },
+  { name: 'Kuroda', image: kuroda },
+  { name: 'Nagashima', image: nagashima },
+  { name: 'Nikaido', image: nikaido },
+  { name: 'Odokawa', image: odokawa },
+  { name: 'Sekiguchi', image: sekiguchi },
+  { name: 'Shibagaki', image: shibagaki },
+  { name: 'Shirakawa', image: shirakawa },
+  { name: 'Tanaka', image: tanaka },
+  { name: 'Wadagaki', image: wadagaki },
+  { name: 'Yamamoto', image: yamamoto },
+  { name: 'Yano', image: yano },
+];
+
+export default initialAnimals;
