254d09525cf169addf480ac3e85e16eb4f233d15
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-03-27 22:33:04 -0600

    Add winner message and image

--- src/components/ScoreBoard.jsx
+++ src/components/ScoreBoard.jsx
@@ -1,4 +1,5 @@
 import '../styles/ScoreBoard.css';
+import congratulation from '../assets/congratulation.gif';
 
 function ScoreBoard({ currentScore, highScore }) {
   return (
@@ -9,7 +10,14 @@ function ScoreBoard({ currentScore, highScore }) {
           <p>High-Score: {highScore}</p>
         </>
       ) : (
-        <p>You win!</p>
+        <>
+          <img src={congratulation} alt="Picture of all Odd Taxi characters" />
+          <p>
+            &quot;When someone asks me a question, I come up with about five
+            possible answers. It takes time for me to choose the best answer
+            that won&apos;t also offend anyone.&quot;
+          </p>
+        </>
       )}
     </div>
   );
